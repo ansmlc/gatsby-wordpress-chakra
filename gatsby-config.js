dd889ff337848f34d92a20762df0441a3741a649
ansmlc <anes.mulalic@outlook.com>
2023-03-24 11:54:23 +0100

    update config to include new packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+const { NODE_ENV } = process.env
+require("dotenv").config({
+  path: `.env.${NODE_ENV}`,
+})
 /**
  * Configure your Gatsby site with this file.
  *
@@ -9,12 +13,55 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
-  },
-  plugins: [
+    title: `Woby`,
+    description: `Gatsby WordPress Starter.`,
+    author: `woby`,
+    siteUrl: `https://woby.netlify.app`,
+    // Social icons URLs / If not using leave it empty ( `` )
+    socialLinks: {
+      facebook:  `https://facebook.com`,
+      instagram:  `https://instagram.com`,
+      linkedin: `https://linkedin.com`,
+      youtube: `https://youtube.com`,
+      twitter: `https://twitter.com`,
+    },
+    frontpageIntro: {
+      firstTagline: 'Your favourite CMS',
+      secondTagline: 'meets JAMStack',
+      description: 'Use WordPress along with Gatsby, to manage your content via worlds post popular CMS, and deploy it on a blazing fast front-end'
+    },
+  }, 
+  plugins: [ 
+    {
+      resolve: "gatsby-plugin-mailchimp",
+      options: {
+        endpoint: 'https://racunarstvo.us20.list-manage.com/subscribe/post?u=4754c309a8f1f5ec52db9ea43&amp;id=adbb10ee9a',
+        timeout: 3500,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-anchor-links",
+      options: {
+        offset: -100
+      }
+    },
+    {
+      resolve: "@chakra-ui/gatsby-plugin",
+        options: {
+          resetCSS: true,
+          isUsingColorMode: true,
+        },
+    },
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+       
+         // replace this url with the url of your wordpress install
+         // http://127.0.0.1/wpgatsby/graphql
+
+         url: `https://limomet.a2hosted.com/websby/graphql`,
+      },
+    },
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -23,21 +70,48 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        defaults: {
+          formats: [`auto`, `webp`],
+          placeholder: `dominantColor`,
+          quality: 82,
+          breakpoints: [750, 1080, 1366, 1920],
+          backgroundColor: `transparent`,
+          tracedSVGOptions: {},
+          blurredOptions: {},
+          jpgOptions: {},
+          pngOptions: {},
+          webpOptions: {},
+          avifOptions: {},
+        }
+      }
+    },
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `gatsby-starter-woby`,
+        short_name: `woby`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#F4F5F6`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        theme_color: `#f56565`,
+        display: `standalone`,
+        icon: `src/static/images/gatsbypress-icon.png`,
+        icon_options: {
+          // For all the options available,
+          // please see the section "Additional Resources" below.
+          purpose: `any maskable`,
+        },      
       },
     },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    `gatsby-plugin-offline`,
   ],
 }
